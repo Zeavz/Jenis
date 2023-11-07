afa063f261161dd34c5579aed764401eac998ffa
Jenis Jesuratnam <jenis.jesuratnam@hotmail.com>
2023-11-06 16:49:05 -0800

    chore: update main description

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Jenis Jesuratnam</h1>
       <div className="card">
         <p>
-          Welcome to my website! I am a software engineer based in Toronto,
+          Welcome to my website! I am a software engineer and manager based in Toronto, leading teams to build incredible performant web applications.
         </p>
       </div>
     </>
