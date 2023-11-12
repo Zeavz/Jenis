9a25d16d6a9e4e7f420c07ae5df9045cef8118d4
Jenis Jesuratnam <jenis.jesuratnam@hotmail.com>
2023-11-11 21:23:40 -0500

    feat: add expert list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {TypeAnimation} from "react-type-animation";
 import './App.css'
 
 function App() {
@@ -8,6 +9,31 @@ function App() {
         <p>
           Welcome to my website! I am a software engineer and manager based in Toronto, leading teams to build incredible performant web applications.
         </p>
+        <TypeAnimation
+            sequence={[
+                'An expert in',
+                1000,
+                'An expert in React',
+                1000,
+                'An expert in Node',
+                1000,
+                'An expert in TypeScript',
+                1000,
+                'An expert in Java',
+                1000,
+                'An expert in Python',
+                1000,
+                'An expert in GraphQL',
+                1000,
+                'An expert in AWS',
+                1000,
+                'An expert in Docker',
+            ]}
+            wrapper="span"
+            speed={50}
+            preRenderFirstString={true}
+            repeat={Infinity}
+        />
       </div>
     </>
   )
